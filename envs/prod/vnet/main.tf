d1949f2ea603bb56915ca77dbf726152bd865550
Marcin Tracz <tracz.marcin7@gmail.com>
2025-09-06 22:50:14 +0200

    Add vnet

---
+++ envs/prod/vnet/main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.name}-rg"
+  location = var.location
+}
+
+module "vnet" {
+  source                  = "../../../modules/vnet/"
+  name                    = var.name
+  location                = azurerm_resource_group.rg.location
+  address_space           = var.address_space
+  resource_group_name     = azurerm_resource_group.rg.name
+  private_subnets         = var.private_subnets
+  public_subnets          = var.public_subnets
+  public_database_subnets = var.public_database_subnets
+}
---
+++ envs/prod/vnet/variables.tf
@@ -0,0 +1,37 @@
+variable "name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "address_space" {
+  description = "Address space for the virtual network"
+  type        = list(string)
+}
+variable "location" {
+  description = "Location for the resources"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "Map of private subnets configuration"
+  type = map(object({
+    address_prefix = string
+  }))
+}
+
+variable "public_subnets" {
+  description = "Map of public subnets configuration"
+  type = map(object({
+    address_prefix = string
+  }))
+}
+
+variable "public_database_subnets" {
+  description = "Map of public subnets configuration"
+  type = map(object({
+    address_prefix = string
+  }))
+}
